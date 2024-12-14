61269e64b6fa7d1e7f48143c8eb07e0920c41ef0
Amine <84468278+amineatm@users.noreply.github.com>
2024-12-14 18:29:37 +0100

    Add validation

--- AuthECClientApp/src/app/user/registration/registration.component.ts
+++ AuthECClientApp/src/app/user/registration/registration.component.ts
@@ -1,27 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './registration.component.html',
   styles: ``,
 })
-export class RegistrationComponent implements OnInit {
-  form!: FormGroup;
+export class RegistrationComponent {
+  constructor(public formBuilder: FormBuilder) {}
+  isSubmitted: boolean = false;
 
-  constructor(private formBuilder: FormBuilder) {}
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
 
-  ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      fullname: [''],
-      email: [''],
-      password: [''],
+    if (password && confirmPassword && password.value != confirmPassword.value)
+      confirmPassword?.setErrors({ passwordMismatch: true });
+    else confirmPassword?.setErrors(null);
+
+    return null;
+  };
+
+  form = this.formBuilder.group(
+    {
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/),
+        ],
+      ],
       confirmPassword: [''],
-    });
-  }
+    },
+    { validators: this.passwordMatchValidator }
+  );
 
   onSubmit() {
+    this.isSubmitted = true;
     console.log(this.form.value);
+  }
+
+  hasDisplayableErrors(controlName: string): boolean {
+    const control = this.form.get(controlName);
+    return (
+      Boolean(control?.invalid) &&
+      (this.isSubmitted || Boolean(control?.touched))
+    );
   }
 }
