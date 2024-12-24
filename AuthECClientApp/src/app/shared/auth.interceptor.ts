d82b2c6daa4842b5e572b2685a026dc2afa9e196
mohamedamine.atmani <mohamedamine.atmani@ax-lab.com>
2024-12-24 14:19:44 +0100

    Add api key to the inerceptor and to env file

--- AuthECClientApp/src/app/shared/auth.interceptor.ts
+++ AuthECClientApp/src/app/shared/auth.interceptor.ts
@@ -1,37 +1,44 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { AuthService } from './services/auth.service';
-import { tap } from 'rxjs';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { tap } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { AuthService } from './services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const router = inject(Router);
   const toastr = inject(ToastrService);
 
+  let clonedReq = req.clone({
+    headers: req.headers.set('Api-Key', environment.ANGULAR_APP_API_KEY),
+  });
+
   if (authService.isLoggedIn()) {
-    const clonedReq = req.clone({
-      headers: req.headers.set(
+    clonedReq = clonedReq.clone({
+      headers: clonedReq.headers.set(
         'Authorization',
         'Bearer ' + authService.getToken()
       ),
     });
-    return next(clonedReq).pipe(
-      tap({
-        error: (err: any) => {
-          if (err.status == 401) {
-            authService.deleteToken();
-            setTimeout(() => {
-              toastr.info('Please login again', 'Session Expired!');
-            }, 1500);
-            router.navigateByUrl('/login  ');
-          } else if (err.status == 403)
-            toastr.error(
-              "Oops! It seems you're not authorized to perform the action."
-            );
-        },
-      })
-    );
-  } else return next(req);
+  }
+
+  return next(clonedReq).pipe(
+    tap({
+      error: (err: any) => {
+        if (err.status == 401) {
+          authService.deleteToken();
+          setTimeout(() => {
+            toastr.info('Please login again', 'Session Expired!');
+          }, 1500);
+          router.navigateByUrl('/login');
+        } else if (err.status == 403) {
+          toastr.error(
+            "Oops! It seems you're not authorized to perform the action."
+          );
+        }
+      },
+    })
+  );
 };
--- AuthECClientApp/src/environments/environment.development.ts
+++ AuthECClientApp/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'https://localhost:7183/api/',
+  API_BASE_URL: 'https://localhost:7183/api/',
+  ANGULAR_APP_API_KEY: 'MyApiKey123',
 };
